47c7bbe9922656f0ec525d9f0d74bffeff4dd5fd
joelton matos <joeltonmatos@gmail.com>
2020-04-20 18:37:31 -0300

    feat: Add actions of timer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,36 +1,25 @@
 import * as api from '../api';
 
-import { CALL_API } from '../middleware/api';
-
-export const FETCH_TASKS_STARTED = 'FETCH_TASKS_STARTED';
-export const FETCH_TASKS_SUCCEEDED = 'FETCH_TASKS_SUCCEEDED';
-export const FETCH_TASKS_FAILURE = 'FETCH_TASKS_FAILURE';
-
 export function fetchTasks() {
   return {
-    [CALL_API]: {
-      types: [FETCH_TASKS_STARTED, FETCH_TASKS_SUCCEEDED, FETCH_TASKS_FAILURE],
-      endpoint: '/tasks',
+    type: 'FETCH_TASKS_STARTED',
+  };
+}
+
+function createTaskSucceeded(task) {
+  return {
+    type: 'CREATE_TASK_SUCCEEDED',
+    payload: {
+      task,
     },
   };
 }
 
-export const CREATE_TASK_STARTED = 'CREATE_TASK_STARTED';
-export const CREATE_TASK_SUCCEEDED = 'CREATE_TASK_SUCCEEDED';
-export const CREATE_TASK_FAILED = 'CREATE_TASK_FAILED';
-
 export function createTask({ title, description, status = 'Unstarted' }) {
-  return {
-    [CALL_API]: {
-      types: [CREATE_TASK_STARTED, CREATE_TASK_SUCCEEDED, CREATE_TASK_FAILED],
-      endpoint: '/tasks',
-      method: 'POST',
-      body: {
-        title,
-        description,
-        status,
-      },
-    },
+  return dispatch => {
+    api.createTask({ title, description, status }).then(resp => {
+      dispatch(createTaskSucceeded(resp.data));
+    });
   };
 }
 
@@ -43,6 +32,14 @@ function editTaskSucceeded(task) {
   };
 }
 
+function progressTimerStart(taskId) {
+  return { type: 'TIMER_STARTED', payload: { taskId } };
+}
+
+function progressTimerStop(taskId) {
+  return { type: 'TIMER_STOPPED', payload: { taskId } };
+}
+
 export function editTask(id, params = {}) {
   return (dispatch, getState) => {
     const task = getTaskById(getState().tasks.tasks, id);
@@ -52,6 +49,16 @@ export function editTask(id, params = {}) {
     };
     api.editTask(id, updatedTask).then(resp => {
       dispatch(editTaskSucceeded(resp.data));
+
+      // if task moves into "In Progress", start timer
+      if (resp.data.status === 'In Progress') {
+        return dispatch(progressTimerStart(resp.data.id));
+      }
+
+      // if tasks move out of "In Progress", stop timer
+      if (task.status === 'In Progress') {
+        return dispatch(progressTimerStop(resp.data.id));
+      }
     });
   };
 }
